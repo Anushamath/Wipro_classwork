bca8898a2cd701cf0292ec0517769d54e10c8d9f
Anushamath <anushamath20@gmail.com>
2025-09-11 21:06:17 +0530

    Add files via upload

---
+++ Day4_Programs/Day4_Programs/src/com/example/exceptions/customexceptions/InsufficientFundsException.java
@@ -0,0 +1,14 @@
+package com.example.exceptions.customexceptions;
+
+public class InsufficientFundsException extends Exception {
+	
+	public InsufficientFundsException(String message)
+	{
+		
+		super(message);
+		
+	}
+	
+	
+
+}
